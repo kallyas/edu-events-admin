1e5066346b77c9895d04e220a4db642d5133f981
Kally <kallyasmedia@gmail.com>
2021-09-03 15:25:01 +0000

    ft(login): create login handler

---
+++ src/service/AuthService.js
@@ -0,0 +1,15 @@
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+import app from "../config/firebaseInit"
+
+
+export default async function loginUser(data) {
+    const auth = getAuth();
+    try {
+        const signIn = await signInWithEmailAndPassword(auth, data.email, data.password)
+        const user = signIn.user;
+        return user.stsTokenManager
+    } catch (error) {
+        return error.message
+    }
+}
