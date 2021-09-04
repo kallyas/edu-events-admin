61d4f645014e517dce11c10c0f25ad998eb238b1
Kally <kallyasmedia@gmail.com>
2021-09-04 16:40:21 +0000

    Add image upload function

---
+++ src/components/ImageUpload.js
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+import { Row, Col, Card, Form, Image } from "@themesberg/react-bootstrap";
+import { useDropzone } from "react-dropzone";
+import UploadImageService from "../service/UploadImageService";
+
+const ImageUpload = () => {
+    const [files, setFiles] = useState([]);
+    const { getRootProps, getInputProps } = useDropzone({
+      accept: 'image/*',
+      onDrop: files => setFiles(files.map(file => ({
+        ...file,
+        preview: URL.createObjectURL(file)
+      })))
+    });
+  
+    const ImageUploadFile = (props) => {
+      const { path, preview } = props;
+  
+      return (
+        <Col xs={3} className="dropzone-preview">
+          <Image src={preview} className="dropzone-image" />
+          <Card.Text className="dropzone-filename">
+            {path}
+          </Card.Text>
+        </Col>
+      );
+    };
+  
+  useEffect(() => {
+    UploadImageService(files[0])
+  }, [files])
+  
+    return (
+      <>
+        <Form {...getRootProps({ className: "dropzone rounded d-flex align-items-center justify-content-center mb-4" })}>
+          <Form.Control {...getInputProps()} />
+          <div className="dz-default dz-message text-center">
+            <p className="dz-button mb-0">Drop event image header file here to upload</p>
+          </div>
+        </Form>
+        <Row className="dropzone-files">
+          {files.map(file => <ImageUploadFile key={file.path} {...file} />)}
+        </Row>
+      </>
+    );
+  };
+  
+  export default ImageUpload
