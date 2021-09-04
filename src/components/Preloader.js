b8bc27c605855beb48c745f4ce6ae4a4a83689ea
Kally <kallyasmedia@gmail.com>
2021-09-04 16:40:56 +0000

    add preloader

---
+++ src/components/Preloader.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import { Image } from '@themesberg/react-bootstrap';
+
+import ReactLogo from "../assets/images/react-logo-transparent.svg";
+
+const Preloader =  (props) => {
+
+  const { show } = props;
+
+  return (
+    <div className={`preloader bg-soft flex-column justify-content-center align-items-center ${show ? "" : "show"}`}>
+      <Image className="loader-element animate__animated animate__jackInTheBox" src={ReactLogo} height={40} />
+    </div>
+  );
+};
+
+export default Preloader
