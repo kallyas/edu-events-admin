da95a20b7689e47d8908665b2852647d30cfe5a1
Kally <kallyasmedia@gmail.com>
2021-09-04 16:37:57 +0000

    add data parts

---
+++ src/data/notifications.js
@@ -0,0 +1,17 @@
+/* eslint-disable import/no-anonymous-default-export */
+
+import Profile1 from "../assets/images/profile-picture-1.jpg"
+
+const notifications = [
+    {
+        "id": 1,
+        "read": false,
+        "image": Profile1,
+        "sender": "Test user",
+        "time": "a few moments ago",
+        "link": "#",
+        "message": `Test Message`
+    }
+]
+
+export default notifications
