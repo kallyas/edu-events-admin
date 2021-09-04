bd6fe4a8b13adb878688f098f448fd5b25f3c2df
Kally <kallyasmedia@gmail.com>
2021-09-04 16:42:58 +0000

    add scroll to top function

---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+import ScrollToTop from './ScrollToTop'
+import Preloader from './Preloader'
+import Footer from './Footer'
+import Navbar from './Navbar'
+import Sidebar from './Sidebar'
+
+
+export {
+    ScrollToTop,
+    Preloader,
+    Footer,
+    Navbar,
+    Sidebar
+}
