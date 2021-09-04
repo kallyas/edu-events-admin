ed9040a21bb30a102d06b76b7da62ff120e48fe1
Kally <kallyasmedia@gmail.com>
2021-09-04 16:43:25 +0000

    config app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 /* eslint-disable no-unused-vars */
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Dashboard from "./pages/Dashboard";
+import Events from "./pages/events";
 import Login from './pages/Login';
+import NewEvent from "./pages/NewEvent";
+import { Routes } from "./routes";
+
+require('dotenv').config()
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/auth/login" component={Login} />
-        <Route exact path="/">
-          <h2>Admin area</h2>
-        </Route>
-        {/* <Route exact path="/dashboard" component={Dashboard} /> */}
+        <Route exact path={Routes.Login.path} component={Login} />
+        <ProtectedRoute exact path={Routes.Dashboard.path} component={Dashboard} />
+        <ProtectedRoute exact path={Routes.AddEvent.path} component={NewEvent} />
+        <ProtectedRoute exact path={Routes.Events.path} component={Events} />
       </Switch>
     </BrowserRouter>
   );
--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-import "bootstrap/dist/css/bootstrap.min.css";
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter } from "react-router-dom";
+
+// core styles
+import "./scss/volt.scss";
+import "@fortawesome/fontawesome-free/css/all.css";
+import "react-datetime/css/react-datetime.css";
+
+
 import App from './App';
+import { ScrollToTop } from "./components/index"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HashRouter basename="/">
+      <ScrollToTop />
+      <App />
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
