7213dafbcdd99d05f1bb042060f5cd8b38ad3acc
Kally <kallyasmedia@gmail.com>
2021-09-04 16:35:52 +0000

    Add new events page

---
+++ src/pages/NewEvent.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/no-anonymous-default-export */
+import { useState, useEffect } from 'react'
+import { Col, Row } from '@themesberg/react-bootstrap';
+import EventForm from "../components/EventForm"
+import ImageUpload from '../components/ImageUpload';
+
+const NewEvent = () => {
+    return (
+        <>
+      <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center py-4">
+        <div className="d-flex">
+        </div>
+      </div>
+
+      <Row>
+        <Col xs={12} xl={8}>
+         <EventForm />
+        </Col>
+
+        <Col xs={12} xl={4}>
+          <Row>
+            <Col xs={12}>
+              <ImageUpload />
+            </Col>
+            <Col xs={12}>
+             {/* {} */}
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </>
+    )
+}
+
+export default NewEvent
