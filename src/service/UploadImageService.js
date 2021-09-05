ece9a484b56a0a22b140569ae5128d8908508ac3
Kally <kallyasmedia@gmail.com>
2021-09-05 07:15:32 +0000

    fix image upload bug

--- src/service/UploadImageService.js
+++ src/service/UploadImageService.js
@@ -1,39 +1,46 @@
 /* eslint-disable no-unused-vars */
-import app from '../config/firebaseInit'
-import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import app from '../config/firebaseInit';
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from 'firebase/storage';
 
-const storage = getStorage()
+const storage = getStorage();
 
 export default async function UploadImageService(image) {
-    const storageRef = ref(storage, `events/${image?.path}`)
-    const uploadTask = uploadBytesResumable(storageRef, image);
+  const storageRef = ref(storage, `events/${image?.name}`);
+  const uploadTask = uploadBytesResumable(storageRef, image);
 
-    uploadTask.on('state_changed', 
-  (snapshot) => {
-    // Observe state change events such as progress, pause, and resume
-    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-    console.log('Upload is ' + progress + '% done');
-    switch (snapshot.state) {
-      case 'paused':
-        console.log('Upload is paused');
-        break;
-      case 'running':
-        console.log('Upload is running');
-        break;
-      default:
-          console.log('unknown error')
+  uploadTask.on(
+    'state_changed',
+    (snapshot) => {
+      // Observe state change events such as progress, pause, and resume
+      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log('Upload is ' + progress + '% done');
+      switch (snapshot.state) {
+        case 'paused':
+          console.log('Upload is paused');
+          break;
+        case 'running':
+          console.log('Upload is running');
+          break;
+        default:
+          console.log('unknown error');
+      }
+    },
+    (error) => {
+      // Handle unsuccessful uploads
+    },
+    () => {
+      // Handle successful uploads on complete
+      // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+        console.log('File available at', downloadURL);
+        localStorage.setItem('imgUrl', downloadURL);
+      });
     }
-  }, 
-  (error) => {
-    // Handle unsuccessful uploads
-  }, 
-  () => {
-    // Handle successful uploads on complete
-    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-      console.log('File available at', downloadURL);
-    });
-  }
-);
+  );
 }
