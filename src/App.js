adbf30f64e5d02c0d8f490078becfa503ef8179c
Kally <kallyasmedia@gmail.com>
2021-09-05 07:16:59 +0000

    add 404 route

--- src/App.js
+++ src/App.js
@@ -5,18 +5,24 @@ import Dashboard from "./pages/Dashboard";
 import Events from "./pages/events";
 import Login from './pages/Login';
 import NewEvent from "./pages/NewEvent";
+import NotFound from "./pages/NotFound";
 import { Routes } from "./routes";
+import useToken from "./utils/useToken";
 
 require('dotenv').config()
 
 function App() {
+  const { token } = useToken();
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path={Routes.Login.path} component={Login} />
+        <Route exact path={Routes.Login.path} >
+          { token ? <Redirect to={Routes.Dashboard.path} /> : <Login />}
+        </Route>
         <ProtectedRoute exact path={Routes.Dashboard.path} component={Dashboard} />
         <ProtectedRoute exact path={Routes.AddEvent.path} component={NewEvent} />
         <ProtectedRoute exact path={Routes.Events.path} component={Events} />
+        <Route component={NotFound} />
       </Switch>
     </BrowserRouter>
   );
