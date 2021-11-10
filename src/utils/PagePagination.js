af69f19aaf81047ba303b50eabcf6b657cc619ae
kallyas <kallyasmedia@gmail.com>
2021-11-10 06:53:44 +0000

    fix: pagination3

--- src/utils/PagePagination.js
+++ src/utils/PagePagination.js
@@ -5,8 +5,8 @@ import { faAngleDoubleLeft, faAngleDoubleRight } from "@fortawesome/free-solid-s
 
 
 const PagePagination = (props) => {
-    const [activeItem, setActiveItem] = useState(props.itemsPerPage);
-    const { totalPages = props.totalItems, size = "md", withIcons = false, disablePrev = false } = props;
+    const [activeItem, setActiveItem] = useState(1);
+    const { totalPages = props.itemsPerPage, size = "md", withIcons = false, disablePrev = false } = props;
   
     const onPrevItem = () => {
       const prevActiveItem = activeItem === 1 ? activeItem : activeItem - 1;
