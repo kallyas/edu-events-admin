24616984a591bebd95579c30f4ebb55e2f48811b
Kally <60466044+kallyas@users.noreply.github.com>
2022-02-16 18:08:36 +0300

    fix: test url

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,6 +1,6 @@
 describe('login into the admin area', () => {
     it('should login sucessfully into the admin area', () => {
-        cy.visit('https://edu-evets-admin.netlify.app/')
+        cy.visit('https://outboxedu-admin.netlify.app/')
         cy.get('input[type=email]').type('admin@outbox.co.ug')
         cy.get('input[type=password]').type('testpassword')
         cy.findByRole('button', { name: /login/i }).click()
@@ -11,7 +11,7 @@ describe('login into the admin area', () => {
 
 describe('login error', () => {
     it('should show login error', () => {
-        cy.visit('https://edu-evets-admin.netlify.app/')
+        cy.visit('https://outboxedu-admin.netlify.app/')
         cy.findByRole('button', { name: /login/i }).click()
         cy.findByRole('heading', {name: /error/i }).contains('Error')
     })
