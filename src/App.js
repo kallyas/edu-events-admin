cc0972dc67e0ace24a809ccf968d400bff07175f
Kally <kallyasmedia@gmail.com>
2022-03-28 11:38:51 +0000

    fix: remove old template

--- src/App.js
+++ src/App.js
@@ -1,64 +1,13 @@
 /* eslint-disable no-unused-vars */
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import ProtectedRoute from "./components/ProtectedRoute";
 import { AuthProvider } from "./context/AuthContext";
-import {
-  Dashboard,
-  Events,
-  Login,
-  NewEvent,
-  NotFound,
-  UserList,
-  Addproject,
-  EditEvent,
-  Enrollment,
-  EnrollmentStudent,
-} from "./pages/index";
-import { Routes } from "./routes";
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
         <Switch>
-          <Route exact path={Routes.Login.path} component={Login} />
-          <ProtectedRoute
-            exact
-            path={Routes.Dashboard.path}
-            component={Dashboard}
-          />
-          <ProtectedRoute
-            exact
-            path={Routes.AddEvent.path}
-            component={NewEvent}
-          />
-          <ProtectedRoute exact path={Routes.Events.path} component={Events} />
-          <ProtectedRoute
-            exact
-            path={Routes.UserList.path}
-            component={UserList}
-          />
-          <ProtectedRoute
-            exact
-            path={Routes.Addproject.path}
-            component={Addproject}
-          />
-          <ProtectedRoute
-            exact
-            path={`${Routes.EditProject.path}/:id`}
-            component={EditEvent}
-          />
-          <ProtectedRoute
-            exact
-            path={Routes.Enrollment.path}
-            component={Enrollment}
-          />
-           <ProtectedRoute
-            exact
-            path={`${Routes.EnrollmentStudent.path}/:id`}
-            component={EnrollmentStudent}
-          />
-          <Route component={NotFound} />
+          {/* Add routes here */}
         </Switch>
       </AuthProvider>
     </BrowserRouter>
--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-import ScrollToTop from "./ScrollToTop";
-import Preloader from "./Preloader";
-import Footer from "./Footer";
-import Navbar from "./Navbar";
-import Sidebar from "./Sidebar";
-import EnrollmentTable from "./EnrollmentTable";
-
-export { ScrollToTop, Preloader, Footer, Navbar, Sidebar, EnrollmentTable };
