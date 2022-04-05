2ff7df57cc6c183d5d53f9103b7e052c16d2ecee
Kally <kallyasmedia@gmail.com>
2022-04-05 23:09:30 +0000

    fix: hash router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 // core styles
 import "./assets/dist/css/tabler.min.css";
@@ -14,11 +13,9 @@ import store from "./store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter basename="/">
       <Provider store={store}>
         <App />
       </Provider>
-    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
