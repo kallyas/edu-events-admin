e35c832231d13f91ffbe2cb5825d529bdf2fc7a3
Ivan Epou <ivanepou92@gmail.com>
2022-04-06 15:22:26 +0300

    Rename css files and folders

--- src/index.js
+++ src/index.js
@@ -2,20 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
 // core styles
-import "./assets/dist/css/tabler.min.css";
+import "./assets/css/tabler.min.css";
 // import "./assets/dist/css/tabler-flags.min.css";
 // import "./assets/dist/css/tabler-payments.min.css";
-import "./assets/dist/css/tabler-vendors.min.css";
-import "./assets/dist/css/tabler-custom.min.css";
+import "./assets/css/tabler-vendors.min.css";
+import "./assets/css/tabler-custom.min.css";
 import "./App.css";
 import App from './App';
 import store from "./store";
 
 ReactDOM.render(
   <React.StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
