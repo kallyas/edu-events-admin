cd7376fd897d1fbabc74327f0d2cd40a3199f883
kallyas <kallyasmedia@gmail.com>
2022-04-08 21:09:16 +0300

    fix: fetching data from firebase

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,114 +7,110 @@ import { useEffect } from 'react';
 import { IconEye } from '@tabler/icons';
 
 const Login = () => {
-    const navigate = useNavigate();
-    const { login, currentUser } = useAuth();
-    const [loading, setLoading] = useState(false);
-    const [show, setShow] = useState(false);
-    const [data, setData] = useState({
-        email: '',
-        password: '',
-    });
+  const navigate = useNavigate();
+  const { login, currentUser } = useAuth();
+  const [loading, setLoading] = useState(false);
+  const [show, setShow] = useState(false);
+  const [data, setData] = useState({
+    email: '',
+    password: '',
+  });
 
-    const handleChange = (e) => {
-        setData({ ...data, [e.target.name]: e.target.value });
-    };
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
 
-    //show password
-    const handleClickShowPassword = () => {
-        setShow(!show);
-    };
+  //show password
+  const handleClickShowPassword = () => {
+    setShow(!show);
+  };
 
-    //handle submit
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            setLoading(true);
-            await login(data);
-            navigate('/dashboard');
-        } catch (err) {
-            console.log(err);
-        }
-        setLoading(false);
-    };
+  //handle submit
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      await login(data);
+      navigate('/dashboard');
+    } catch (err) {
+      console.log(err);
+    }
+    setLoading(false);
+  };
 
-    useEffect(() => {
-        if (currentUser) {
-            navigate('/dashboard');
-        }
-    }, [currentUser, navigate]);
+  useEffect(() => {
+    if (currentUser) {
+      navigate('/dashboard');
+    }
+  }, [currentUser, navigate]);
 
-    return (
-        <div>
-            <div className="page page-center">
-                <div className="container-tight py-1">
-                    <form
-                        className="card card-md login-form"
-                        autoComplete="off"
-                        onSubmit={handleSubmit}
+  return (
+    <div>
+      <div className="page page-center">
+        <div className="container-tight py-1">
+          <form className="card card-md login-form" autoComplete="off" onSubmit={handleSubmit}>
+            <div className="card-body">
+              <div className="text-center mb-2 company-logo">
+                <img src={eduLogo} height="50" alt="" />
+              </div>
+              <div className="mb-3">
+                <label className="form-label">Email address</label>
+                <input
+                  type="email"
+                  name="email"
+                  className="form-control"
+                  placeholder="Enter Email"
+                  value={data.email}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="mb-2">
+                <label className="form-label">
+                  Password
+                  <span className="form-label-description">
+                    <Link to="/"> I forgot password </Link>
+                  </span>
+                </label>
+                <div className="input-group input-group-flat">
+                  <input
+                    type={show ? 'text' : 'password'}
+                    name="password"
+                    className="form-control"
+                    placeholder="Enter Password"
+                    autoComplete="off"
+                    value={data.password}
+                    onChange={handleChange}
+                  />
+                  <span className="input-group-text">
+                    <Link
+                      to="/"
+                      className="link-secondary"
+                      title="Show password"
+                      data-bs-toggle="tooltip"
+                      onClick={handleClickShowPassword}
                     >
-                        <div className="card-body">
-                            <div className="text-center mb-2 company-logo">
-                                <img src={eduLogo} height="50" alt="" />
-                            </div>
-                            <div className="mb-3">
-                                <label className="form-label">Email address</label>
-                                <input
-                                    type="email"
-                                    name="email"
-                                    className="form-control"
-                                    placeholder="Enter Email"
-                                    value={data.email}
-                                    onChange={handleChange}
-                                />
-                            </div>
-                            <div className="mb-2">
-                                <label className="form-label">
-                                    Password
-                                    <span className="form-label-description">
-                                        <Link to="/"> I forgot password </Link>
-                                    </span>
-                                </label>
-                                <div className="input-group input-group-flat">
-                                    <input
-                                        type={show ? 'text' : 'password'}
-                                        name='password'
-                                        className="form-control"
-                                        placeholder="Enter Password"
-                                        autoComplete="off"
-                                        value={data.password}
-                                        onChange={handleChange}
-                                    />
-                                    <span className="input-group-text">
-                                        <Link
-                                            to="/"
-                                            className="link-secondary"
-                                            title="Show password"
-                                            data-bs-toggle="tooltip"
-                                            onClick={handleClickShowPassword}
-                                        >
-                                            <IconEye size={24} />
-                                        </Link>
-                                    </span>
-                                </div>
-                            </div>
-                            <div className="mb-2">
-                                <label className="form-check">
-                                    <input type="checkbox" className="form-check-input" />
-                                    <span className="form-check-label"> Remember me on this device </span>
-                                </label>
-                            </div>
-                            <div className="form-footer">
-                                <button type="submit" className="btn btn-primary w-100 login-btn">
-                                    {loading ? 'Logging in...' : 'Sign in'}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                      <IconEye size={24} />
+                    </Link>
+                  </span>
                 </div>
+              </div>
+              <div className="mb-2">
+                <label className="form-check">
+                  <input type="checkbox" className="form-check-input" />
+                  <span className="form-check-label"> Remember me on this device </span>
+                </label>
+              </div>
+              <div className="form-footer">
+                <button type="submit" className="btn btn-primary w-100 login-btn">
+                  {loading ? 'Logging in...' : 'Sign in'}
+                </button>
+              </div>
             </div>
+          </form>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,27 +1,30 @@
-import React from 'react'
-import { Footer, NavBar } from '../components'
-import { useSelector } from 'react-redux'
-import { projectsSelector } from '../features/projects/projectSlice';
+import React, { useEffect } from 'react';
+import { Footer, NavBar } from '../components';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchProjectsAsync, projectsSelector } from '../features/projects/projectSlice';
 
 const Projects = () => {
-    const { projects } = useSelector(projectsSelector);
-    return (
-        <div>
-            <NavBar />
-            <div className="app-body">
-                <h1> Projects </h1>
-                {
-                    projects.map(project => (
-                        <div key={project.id}>
-                            <h2>{project.name}</h2>
-                            <p>{project.description}</p>
-                        </div>
-                    ))
-                }
-            </div>
-            <Footer />
-        </div>
-    )
-}
+  const dispatch = useDispatch();
+  const { projects } = useSelector(projectsSelector);
 
-export default Projects
+  useEffect(() => {
+    dispatch(fetchProjectsAsync());
+  }, [dispatch]);
+  return (
+    <div>
+      <NavBar />
+      <div className="app-body">
+        <h1> Projects </h1>
+        {projects?.map((project) => (
+          <div key={project.id}>
+            <h2>{project.question}</h2>
+            <p>{project.desc}</p>
+          </div>
+        ))}
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Projects;
