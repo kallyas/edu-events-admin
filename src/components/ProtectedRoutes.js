7f8b58b0ea9b27a0a95ed2ab6f5059724d63212a
kallyas <kallyasmedia@gmail.com>
2022-04-15 17:07:12 +0300

    ft: fetch data once we have the user logged in. this will reduce fetching of data on each page

--- src/components/ProtectedRoutes.js
+++ src/components/ProtectedRoutes.js
@@ -1,9 +1,25 @@
+import { useEffect } from 'react'
 import { Navigate, Outlet } from 'react-router-dom';
+import { useDispatch } from 'react-redux'
 import { Routes as routes } from '../routes';
 import { useAuth } from '../context/AuthContext';
+import { fetchUsersAsync } from '../features/users/usersSlice'
+import { getEnrollmentAsync } from '../features/enrollment/enrollmentSlice';
+import { fetchEventsAsync } from '../features/events/eventsSlice';
+
 
 const ProtectedRoutes = () => {
   const { currentUser } = useAuth();
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    if(currentUser) {
+      dispatch(fetchUsersAsync())
+      dispatch(getEnrollmentAsync());
+      dispatch(fetchEventsAsync());
+    }
+  }, [dispatch, currentUser])
+
   return currentUser ? <Outlet /> : <Navigate to={routes.Login.path} />;
 };
 
--- src/pages/Enrollments.jsx
+++ src/pages/Enrollments.jsx
@@ -1,154 +1,160 @@
-import _ from 'lodash'
-import React, { useState, useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Footer, NavBar } from '../components'
-import EnrollmentTable from '../components/EnrollmentTable'
-import ExportLabel from '../components/ExportLabel'
-import SearchBar from '../components/SearchBar'
+import _ from 'lodash';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Footer, NavBar } from '../components';
+import EnrollmentTable from '../components/EnrollmentTable';
+import ExportLabel from '../components/ExportLabel';
+import SearchBar from '../components/SearchBar';
 import PagePagination from '../utils/PagePagination';
-import { paginate } from '../utils/paginate'
+import { paginate } from '../utils/paginate';
 import { enrollmentSelector, getEnrollmentAsync } from './../features/enrollment/enrollmentSlice';
 
 const Enrollments = () => {
-    const dispatch = useDispatch();
-    const [pageSize, setPageSize] = useState(10);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [sortColumn, setSortColumn] = useState({ path: 'firstName', order: 'asc' });
-    const [searchQuery, setSearchQuery] = useState('');
+  const dispatch = useDispatch();
+  const [pageSize, setPageSize] = useState(10);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [sortColumn, setSortColumn] = useState({ path: 'firstName', order: 'asc' });
+  const [searchQuery, setSearchQuery] = useState('');
 
-    useEffect(() => {
-        dispatch(getEnrollmentAsync());
-    }, [dispatch]);
+  const { data: enrollments } = useSelector(enrollmentSelector);
 
-    const { data: enrollments } = useSelector(enrollmentSelector);
+  //handle delete
+  const handleDelete = (id) => {
+    dispatch({
+      payload: id,
+    });
+  };
 
-    //handle delete
-    const handleDelete = (id) => {
-        dispatch({
-            payload: id
-        });
-    }
+  //handle page change
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
 
-    //handle page change
-    const handlePageChange = (pageNumber) => {
-        setCurrentPage(pageNumber);
-    }
+  //handle sort
+  const handleSort = (sortColumn) => {
+    setSortColumn(sortColumn);
+  };
 
-    //handle sort
-    const handleSort = (sortColumn) => {
-        setSortColumn(sortColumn);
-    }
+  //handle search
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+    setCurrentPage(1);
+  };
 
-    //handle search
-    const handleSearch = (query) => {
-        setSearchQuery(query);
-        setCurrentPage(1);
-    }
+  //handle row number
+  const handleRowNumber = (value) => {
+    const newPageSize = parseInt(value);
+    setCurrentPage(1);
+    setPageSize(newPageSize);
+  };
 
-    //handle row number
-    const handleRowNumber = (value) => {
-        const newPageSize = parseInt(value);
-        setCurrentPage(1);
-        setPageSize(newPageSize);
-    }
+  const getData = () => {
+    const allEnrollments = [...enrollments];
+    const filtered = allEnrollments.filter(
+      (enrollment) =>
+        enrollment.firstName.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        enrollment.lastName.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        enrollment.email.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        enrollment.phoneNumber.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        enrollment.education.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        enrollment.employment.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        enrollment.experience.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        enrollment.track.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        enrollment.country.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        enrollment.gender.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
+    const paginated = paginate(sorted, currentPage, pageSize);
+    return { totalCount: filtered.length, data: paginated };
+  };
 
-    const getData = () => {
-        const allEnrollments = [...enrollments];
-        const filtered = allEnrollments.filter(
-            enrollment => enrollment.firstName.toLowerCase().includes(searchQuery.toLowerCase())
-                || enrollment.lastName.toLowerCase().includes(searchQuery.toLowerCase())
-                || enrollment.email.toLowerCase().includes(searchQuery.toLowerCase())
-                || enrollment.phoneNumber.toLowerCase().includes(searchQuery.toLowerCase())
-                || enrollment.education.toLowerCase().includes(searchQuery.toLowerCase())
-                || enrollment.employment.toLowerCase().includes(searchQuery.toLowerCase())
-                || enrollment.experience.toLowerCase().includes(searchQuery.toLowerCase())
-                || enrollment.track.toLowerCase().includes(searchQuery.toLowerCase())
-                || enrollment.country.toLowerCase().includes(searchQuery.toLowerCase())
-                || enrollment.gender.toLowerCase().includes(searchQuery.toLowerCase()));
-        const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
-        const paginated = paginate(sorted, currentPage, pageSize);
-        return { totalCount: filtered.length, data: paginated };
-    }
+  const { totalCount, data: myEnrollments } = getData();
 
-    const { totalCount, data: myEnrollments } = getData();
+  const columns = [
+    { title: 'First Name', path: 'firstName' },
+    { title: 'Last Name', path: 'lastName' },
+    { title: 'Gender', path: 'gender' },
+    { title: 'Email', path: 'email' },
+    { title: 'Phone Number', path: 'phoneNumber' },
+    { title: 'Education', path: 'education' },
+  ];
 
-    const columns = [
-        { title: 'First Name', path: 'firstName' },
-        { title: 'Last Name', path: 'lastName' },
-        { title: 'Gender', path: 'gender' },
-        { title: 'Email', path: 'email' },
-        { title: 'Phone Number', path: 'phoneNumber' },
-        { title: 'Education', path: 'education' }
-    ]
-
-    return (
-        <div className="page">
-            <NavBar />
-            <div className="page-wrapper">
-                <div className="page-body">
-                    <div className="container-xl">
-                        <div className="row row-cards">
-                            <div className="col-12">
-                                <div className="card">
-                                    <div className="card-header card-export">
-                                        <h3 className="card-title text-muted-mod">Enrollment List</h3>
-                                        <ExportLabel data={enrollments} columns={columns} fileName={'EDU - Enrollments'} />
-                                    </div>
-                                    <div className="card-body border-bottom py-3">
-                                        <div className="d-flex">
-                                            <div className="text-muted">
-                                                Show
-                                                <div className="mx-2 d-inline-block">
-                                                    <div className="form-group">
-                                                        <select className="form-control sm text-muted" aria-label=".form-select-lg example" name="rowNumber" onChange={(e) => handleRowNumber(e.target.value)}>
-                                                            <option value="10">10</option>
-                                                            <option value="15">15</option>
-                                                            <option value="20">20</option>
-                                                            <option value="25">25</option>
-                                                            <option value="30">30</option>
-                                                        </select>
-                                                    </div>
-                                                </div>
-                                                entries
-                                            </div>
-                                            <div className="ms-auto d-flex text-muted">
-
-                                                <SearchBar
-                                                    value={searchQuery}
-                                                    onChange={(e) => handleSearch(e.target.value)}
-                                                    type="text"
-                                                    name="searchQuery"
-                                                    label="Search"
-                                                />
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="table-responsive">
-                                        {/* Table */}
-                                        <EnrollmentTable
-                                            enrollments={myEnrollments}
-                                            onDelete={handleDelete}
-                                            onSort={handleSort}
-                                            sortColumn={sortColumn}
-                                        />
-                                    </div>
-                                    {/* pagination */}
-                                    <PagePagination
-                                        itemsCount={totalCount}
-                                        pageSize={pageSize}
-                                        currentPage={currentPage}
-                                        onPageChange={handlePageChange}
-                                    />
-                                </div>
-                            </div>
+  return (
+    <div className="page">
+      <NavBar />
+      <div className="page-wrapper">
+        <div className="page-body">
+          <div className="container-xl">
+            <div className="row row-cards">
+              <div className="col-12">
+                <div className="card">
+                  <div className="card-header card-export">
+                    <h3 className="card-title text-muted-mod">Enrollment List</h3>
+                    <ExportLabel
+                      data={enrollments}
+                      columns={columns}
+                      fileName={'EDU - Enrollments'}
+                    />
+                  </div>
+                  <div className="card-body border-bottom py-3">
+                    <div className="d-flex">
+                      <div className="text-muted">
+                        Show
+                        <div className="mx-2 d-inline-block">
+                          <div className="form-group">
+                            <select
+                              className="form-control sm text-muted"
+                              aria-label=".form-select-lg example"
+                              name="rowNumber"
+                              onChange={(e) => handleRowNumber(e.target.value)}
+                            >
+                              <option value="10">10</option>
+                              <option value="15">15</option>
+                              <option value="20">20</option>
+                              <option value="25">25</option>
+                              <option value="30">30</option>
+                            </select>
+                          </div>
                         </div>
+                        entries
+                      </div>
+                      <div className="ms-auto d-flex text-muted">
+                        <SearchBar
+                          value={searchQuery}
+                          onChange={(e) => handleSearch(e.target.value)}
+                          type="text"
+                          name="searchQuery"
+                          label="Search"
+                        />
+                      </div>
                     </div>
+                  </div>
+                  <div className="table-responsive">
+                    {/* Table */}
+                    <EnrollmentTable
+                      enrollments={myEnrollments}
+                      onDelete={handleDelete}
+                      onSort={handleSort}
+                      sortColumn={sortColumn}
+                    />
+                  </div>
+                  {/* pagination */}
+                  <PagePagination
+                    itemsCount={totalCount}
+                    pageSize={pageSize}
+                    currentPage={currentPage}
+                    onPageChange={handlePageChange}
+                  />
                 </div>
-                {/* Footer */}
-                <Footer />
+              </div>
             </div>
+          </div>
         </div>
-    )
-}
+        {/* Footer */}
+        <Footer />
+      </div>
+    </div>
+  );
+};
 
-export default Enrollments
+export default Enrollments;
